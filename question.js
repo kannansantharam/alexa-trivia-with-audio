1099950239103390fb3e2b53e7b45fdbe321b325
Kannan Krish <as.kannan4@gmail.com>
2017-12-19 09:36:30 +0530

    Create question.js

---
+++ question.js
@@ -0,0 +1,72 @@
+'use strict';
+const Alexa = require('alexa-sdk');
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+
+    QUESTIONS_EN_GB: [
+        {
+            '<audio src="https://s3.amazonaws.com/alexasoul/om.mp3"/>': [
+                '13,000',
+                '1,200',
+                '5,000',
+                '700',
+                '1,000',
+                '120,000',
+            ],
+        },
+    
+    ],
+   QUESTIONS_EN_US: [
+        {
+            
+           'https://s3.amazonaws.com/alexasoul/VICTORY_alexaConverted.mp3': [
+                '13,000',
+                '1,200',
+                '5,000',
+                '700',
+                '1,000',
+                '120,000',
+            ],
+        },
+        {
+            
+           'https://s3.amazonaws.com/alexasoul/VICTORY_alexaConverted.mp3': [
+                '13,000',
+                '1,200',
+                '5,000',
+                '700',
+                '1,000',
+                '120,000',
+            ],
+        },
+        {
+            
+           'https://s3.amazonaws.com/alexasoul/VICTORY_alexaConverted.mp3': [
+                '13,000',
+                '1,200',
+                '5,000',
+                '700',
+                '1,000',
+                '120,000',
+            ],
+        },
+    
+    ],
+    QUESTIONS_DE_DE: [
+        {
+            'Rentiere haben ein sehr dickes Fell. Wie viele Haare pro Quadratzentimeter haben sie?': [
+                '13,000',
+                '1,200',
+                '5,000',
+                '700',
+                '1,000',
+                '120,000',
+            ],
+        },
+       
+    ],
+};
